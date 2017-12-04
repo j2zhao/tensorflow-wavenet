d33545e43014219c78558ffb16377113305a8879
Ubuntu <ubuntu@ip-172-31-30-224.us-east-2.compute.internal>
2017-12-04 03:30:15 +0000

    ubuntu files

--- wavenet/audio_reader.py
+++ wavenet/audio_reader.py
@@ -32,7 +32,7 @@ def randomize_files(files):
         yield files[file_index]
 
 
-def find_files(directory, pattern='*.wav'):
+def find_files(directory, pattern='*.WAV'):
     '''Recursively finds all files matching the pattern.'''
     files = []
     for root, dirnames, filenames in os.walk(directory):
